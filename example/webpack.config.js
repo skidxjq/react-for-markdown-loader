53bc39dcf7872241148736519bd5376d9af1795d
谢家全 <hzxiejiaquan@corp.netease.com>
2017-08-12 16:16:46 +0800

    fix package.json

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const cwd = process.cwd()
 
@@ -20,11 +21,22 @@ module.exports = {
       exclude: /node_modules/
     }, {
       test: /\.(md)$/,
-      use: path.resolve(__dirname, '../lib')
+      loader: path.resolve(__dirname, '../lib')
+    }, {
+      test: /\.(css|scss|sass)$/,
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: [
+          'css-loader', {
+            loader: 'postcss-loader'
+          },
+          'sass-loader?sourceMap'
+        ]
+      })
     }]
   },
   plugins: [
-     new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src/index.html'),
       hash: false,
       filename: path.join(cwd, 'index.html'),
